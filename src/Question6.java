7ad320a9385025a69b277f7b2c7cdafb38da21f1
JIWON-C-33 <202430331@email.daelim.ac.kr>
2024-10-11 14:15:49 +0900

    Question6.java

--- src/Question6.java
+++ src/Question6.java
@@ -2,24 +2,17 @@ import java.util.Scanner;
 
 public class Question6 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("첫 번째 숫자를 입력해주세요: ");
+        int a = sc.nextInt();
+        System.out.println("두 번째 숫자를 입력해주세요: ");
+        int b = sc.nextInt();
 
-        System.out.print("첫 번째 숫자: ");
-        int a = scanner.nextInt();
-        System.out.print("두 번째 숫자: ");
-        int b = scanner.nextInt();
-
-        int gcd = findGCD(a, b);
-
-        System.out.println("두 수의 최대공약수는: " + gcd);
-    }
-
-    public static int findGCD(int a, int b) {
-        while (b != 0) {
-            int temp = b;
+        while(b != 0){
+            int tmp = b;
             b = a % b;
-            a = temp;
+            a = tmp;
         }
-        return a;
+        System.out.println("최대공약수 는 : " + a);
     }
 }
